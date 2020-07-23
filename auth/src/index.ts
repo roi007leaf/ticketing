50908da2dc2673a894b29c4387150d1c0dc197f9
roi.horowitz <roi.horowitz@payboxapp.com>
2020-07-23 11:29:00 +0300

    feature: finish signup service feature: implement jwt for authentication

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -9,10 +10,18 @@ import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
 import NotFoundError from "./errors/not-found-error";
-import DatabaseConnectionError from "./errors/database-connection-error";
+import * as process from "process";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
@@ -23,6 +32,10 @@ app.all("*", async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
       useNewUrlParser: true,
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,7 +3,7 @@ import { body, validationResult } from "express-validator";
 import User from "../models/user";
 import RequestValidationError from "../errors/request-validation-error";
 import BadRequestError from "../errors/bad-request-error";
-
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.post(
@@ -32,6 +32,19 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+    // Store it on the session object
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }

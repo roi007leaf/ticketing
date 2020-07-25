f678371812b9da910b3c05bcc6407e661655543b
roi.horowitz <roi.horowitz@payboxapp.com>
2020-07-25 11:24:36 +0300

    fix: finalize routes with middleware and errors

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
-import express, {Request, Response} from "express";
-import {currentUser} from "../middlewares/current-user";
-import {requireAuth} from "../middlewares/require-auth";
+import express, { Request, Response } from "express";
+import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 

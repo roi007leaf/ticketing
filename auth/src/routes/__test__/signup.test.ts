ede543ace92a49c8a0fd2be781c341b7c089e0bd
roi.horowitz <roi.horowitz@payboxapp.com>
2020-07-25 14:02:43 +0300

    fix: make sure jest can run under cookie session https restriction, finalize sign up tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,63 @@ it("returns a 201 on succesful signup", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 with invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "testtest.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "p",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missinf email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com" })
+    .expect(400);
+  return request(app)
+    .post("/api/users/signup")
+    .send({ password: "password" })
+    .expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password2",
+    })
+    .expect(400);
+});
+
+it("sets a cookie after a successful sign up", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+  // checks the headers of the response
+  expect(response.get("Set-Cookie")).toBeDefined();
+});

6aadb9a514b848d0ef34e836ceffee9f4ba5e7bf
roi.horowitz <roi.horowitz@payboxapp.com>
2020-07-25 11:23:57 +0300

    fix: finalize routes with middleware and errors

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,16 @@
-import express from 'express';
+import express, {Request, Response} from "express";
+import {currentUser} from "../middlewares/current-user";
+import {requireAuth} from "../middlewares/require-auth";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-    res.send("Hi There")
-})
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  requireAuth,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
 
-export {router as currentUserRouter};
+export { router as currentUserRouter };

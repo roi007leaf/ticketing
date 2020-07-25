1392a4ba1d87047bd9b2546768c473b932de2d72
roi.horowitz <roi.horowitz@payboxapp.com>
2020-07-25 13:51:20 +0300

    fix: setup env variable to not fail the test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,7 @@ import { app } from "../app";
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = "asdfasdf";
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
   await mongoose.connect(mongoUri, {
@@ -24,5 +25,3 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
-
-describe("", () => {});

7a8109a45e2e3b07275bd209c747fb8d2fa3e7a9
roi.horowitz <roi.horowitz@payboxapp.com>
2020-07-25 13:49:06 +0300

    feature: implement first test using jest

--- auth/src/middlewares/validate-request.ts
+++ auth/src/middlewares/validate-request.ts
@@ -1,6 +1,6 @@
 import { Response, Request, NextFunction } from "express";
 import { validationResult } from "express-validator";
-import RequestValidationError from "../errors/request-validation-error";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 export const validateRequest = (
   req: Request,

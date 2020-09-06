0d84755a19a362bc18bc956aa2279b28e398e6ac
Roi Horowitz <roi.horowitz@payboxapp.com>
2020-09-06 10:48:31 +0300

    implement tickets tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,9 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
 
+import { currentUser } from '../../common/src/middlewares/current-user';
+import { createTicketRouter } from './routes/new';
+
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -14,6 +17,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();

05dd098ff657b3b16909e4500cdf299af3948c7e
Roi Horowitz <roi.horowitz@payboxapp.com>
2020-09-16 06:27:27 +0300

    implement graceful shutdown in case nats connection closed

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,7 +2,9 @@ import { requireAuth, validateRequest } from '@rhorg/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 import { Ticket } from '../models/ticket';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -19,12 +21,12 @@ router.post(
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
     await ticket.save();
 
-    // new TicketCreatedPublisher(client).publish({
-    //   id: ticket.id,
-    //   title: ticket.title,
-    //   price: ticket.price,
-    //   userId: ticket.userId,
-    // });
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }

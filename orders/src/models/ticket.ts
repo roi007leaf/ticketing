8e7c58bcc6e78ef3901af2e17b6c5e1ff759d33e
Roi Horowitz <roi.horowitz@payboxapp.com>
2020-09-21 09:18:13 +0300

    fix ticket creation

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -41,7 +41,7 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket({ _id: attrs.id, title: attrs.title, price: attrs });
+  return new Ticket({ _id: attrs.id, title: attrs.title, price: attrs.price });
 };
 ticketSchema.methods.isReserved = async function () {
   // this === the ticket document that we just called 'isReserved' on

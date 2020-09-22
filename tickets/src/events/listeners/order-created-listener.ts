9c8e84f1543a6638318649992f841cf5efbfa1a9
Roi Horowitz <roi.horowitz@payboxapp.com>
2020-09-22 08:32:28 +0300

    implement order created listener tests

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,24 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@rhorg/common';
+import { Message } from 'node-nats-streaming';
+
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    ticket.set({ orderId: data.id });
+
+    await ticket.save();
+
+    msg.ack();
+  }
+}

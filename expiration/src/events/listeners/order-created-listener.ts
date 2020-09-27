2346ef6cd35e8bb44f98b47af159be346265b21c
Roi Horowitz <roi.horowitz@payboxapp.com>
2020-09-27 13:14:29 +0300

    implement expiration queue with no delay

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,17 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@rhorg/common';
 import { Message } from 'node-nats-streaming';
 
+import { expirationQueue } from '../../queues/expiration-queue';
 import { queueGroupName } from './queue-group-name';
 
-export class OrcerCreatedListener extends Listener<OrderCreatedEvent> {
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
 
   queueGroupName = queueGroupName;
 
-  onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({ orderId: data.id });
+
+    msg.ack();
+  }
 }
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,3 +1,4 @@
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
@@ -11,18 +12,24 @@ const start = async () => {
     throw new Error('NATS_CLUSTER_ID must be defined');
   }
 
-  await natsWrapper.connect(
-    process.env.NATS_CLUSTER_ID,
-    process.env.NATS_CLIENT_ID,
-    process.env.NATS_URL
-  );
-  natsWrapper.client.on('close', () => {
-    console.log('NATS connection closed');
-    process.exit();
-  });
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
 
-  process.on('SIGINT', () => natsWrapper.client.close()); // Whenever server is about to shutdown close nats
-  process.on('SIGTERM', () => natsWrapper.client.close());
+    process.on('SIGINT', () => natsWrapper.client.close()); // Whenever server is about to shutdown close nats
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 start();
